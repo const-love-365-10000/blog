06f1f4da4345b285a7fe472b4d2797566537f8b1
余烬 <1425816423@qq.com>
2023-01-24 11:10:13 +0800

    modify: 优化阅读人数定时任务

--- pages/api/addReaderNumber.js
+++ pages/api/addReaderNumber.js
@@ -3,17 +3,20 @@ import cron from 'node-cron'
 
 let readerMap = {}
 
-// 每天0：30执行任务
-cron.schedule('30 0 * * *', () => {
-  console.log('执行定时任务')
-  console.log(readerMap)
-  for (const id of Object.keys(readerMap)) {
-    addReaderNumber(id, readerMap[id])
-  }
+// 定时任务
+cron.schedule('05 * * * *', () => {
+  console.log('执行定时任务', readerMap)
+  const arr = Object.keys(readerMap).map((id) => addReaderNumber(id, readerMap[id]))
+  Promise.all(arr.map(fn => fn())).then(res => {
+    console.log(9, res)
+  }).catch((err) => {
+    console.error(8, err)
+  })
   readerMap = {}
 })
 
 export default function handler (req, res) {
+  console.log('readerMap', readerMap)
   if (req.query.pageId) {
     readerMap[req.query.pageId] = (readerMap[req.query.pageId] || 0) + 1
   }
